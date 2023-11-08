fefeb42975cb16f48d177186ca38a36df21b87f9
Mommy Me <mkm0182@yahoo.com>
2023-11-08 13:26:40 -0600

    Added code to api-routes

---
+++ routes/api-routes/thought-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtsById,
+    createThought,
+    deleteThought,
+    updateThoughtById,
+    createReaction,
+    deleteReaction,
+} = require('../../controllers/thought-controller');
+
+router.route('/').get(getAllThoughts).post(createThought);
+
+router.route('/:thoughtId').get(getThoughtsById).put(updateThoughtById).delete(deleteThought);
+
+router.route('/:thoughtId/reaction').post(createReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
