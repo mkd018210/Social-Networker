bbb85bbeb35b3bc8aa7e2a2adfcf10e05b43a6e1
Mommy Me <mkm0182@yahoo.com>
2023-11-08 13:06:19 -0600

    added code to three models

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const { Schema, model, Types } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function(v) {
+                    return /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(v);
+                }
+            }
+        },
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            }
+        ],
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtuals('friendCount').get(function(){
+    return this.friends.lenth;
+});
+
+const User = model('user', userSchema)
+
+module.exports = user
